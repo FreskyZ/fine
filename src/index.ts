de3b707de72eaa1c9052f5c09d4fea96187b9bca
Fresky Han <FreskyZ@outlook.com>
2019-02-01 23:14:19 +0800

    update index, remove config

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,30 @@
-import express from 'express';
 import fs from 'fs';
-import { STATIC_FILES } from './config';
+import path from 'path';
+import express from 'express';
 import ShanghaiBusInfoController from './api/sh-bus-controller';
+
+const STATIC_DIRECTORY = path.join(__dirname, '../static');
+const HTML_FILES = fs.readdirSync(STATIC_DIRECTORY)
+    .filter(filename => path.extname(filename) == '.html')
+    .map(filename => ({
+        name: filename == 'index.html' ? '' : path.basename(filename, '.html'),
+        path: path.join(STATIC_DIRECTORY, filename),
+    }));
+const HTML404 = `<html>
+    <head><title>404!</title></head>
+    <body>404!</body>
+</html>`;
 
 let app = express();
 
-for (const static_file of STATIC_FILES) {
-    app.get('/' + static_file.route, (_request, response) => {
-        fs.readFile('build/static/' + static_file.filename, 'utf-8', (err, data) => {
-            if (err) {
-                response.statusCode = 404;
-                response.send('<html><head><title>~/404</title></head><body>404!</body></html>');
-                console.log('read static file failed: ' + err);
-            } else {
-                response.send(data);
-            }
-        });
+for (const html_file of HTML_FILES) {
+    app.get('/' + html_file.name, (_request, response) => {
+        response.sendFile(html_file.path);
     });
 }
 
 app.use('/api/sh-bus', ShanghaiBusInfoController);
+app.use((request, response, _next) => { if (request.accepts('html')) response.send(HTML404); });
 
 process.on('SIGINT', () => {
     console.log('INFO: received SIGINT, exiting, waiting for server closing');
