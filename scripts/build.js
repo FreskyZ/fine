2ee08aa692d03f063714ddeee6b996231354cdef
Fresky Han <FreskyZ@outlook.com>
2019-02-22 00:20:21 +0800

    try new build.js

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,13 @@
+const child_process = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
 const minimist = require('minimist');
+const ts = require('typescript');
+const webpack = require('webpack');
+const nodePackage = require('../package.json');
+const DirectoryWatcher = require('./dir_watcher.js');
+
 // build index-page --watch
 // build logs-page --watch
 // build blog-app [--dev | --pro]
@@ -16,14 +25,119 @@ const minimist = require('minimist');
 // web page's entry is 'src/client/<name>.ts' and 'src/client/<name>.less'
 // web app's entry is 'src'client<name>/app.tsx'
 
-const target = process.argv[2];
+const projectDirectory = process.cwd();
+const raiseError = message => { console.log(message); process.exit(0); }
 
-const [targetName, targetType] = target == 'server' ? ['server', 'server']
-    : target.endsWith('-page') ? [target.slice(0, -5), 'page']
-    : target.endsWith('-app') ? [target.slice(0, -4), 'app']
-    : (() => { throw new Error('invalid target'); })();
+function checkProcessArguments() {
+    if (process.argv.length < 3) {
+        raiseError(chalk`{red error}: not enough arguments`);
+    }
 
-console.log({ targetName, targetType });
+    const target = process.argv[2];
+    const [name, type] = target == 'server' ? ['server', 'server']
+        : target.endsWith('-page') ? [target.slice(0, -5), 'page']
+        : target.endsWith('-app') ? [target.slice(0, -4), 'app']
+        : (() => { raiseError(chalk`{red error}: invalid target`); })();
 
-const argv = minimist(process.argv.slice(3));
-console.dir(argv);
+    const options = minimist(process.argv.slice(3));
+    return { targetName: name, targetType: type, targetOptions: options };
+}
+
+const typescriptBaseConfig = {
+    module: ts.ModuleKind.CommonJS,
+    allowSyntheticDefaultImports: true,
+    target: ts.ScriptTarget.ES2017,
+    noEmitOnError: true,
+    noErrorTruncation: true,
+    noFallthroughCaseInSwitch: true,
+    noImplicitAny: true,
+    noImplicitReturns: true,
+    noImplicitThis: true,
+    noUnusedLocals: true,
+    noUnusedParameters: true,
+    strict: true,
+    strictFunctionTypes: true,
+    strictBindCallApply: true,
+    strictNullChecks: true,
+    strictPropertyInitialization: true,
+};
+const typescriptServerConfig = {
+    ...typescriptBaseConfig,
+    outDir: './custom_build',
+};
+
+function buildServer(options) {
+     
+    // NOTE: according to source code, createProgramOptions.options is used instead of CompilerHost.options
+    const host = ts.createCompilerHost({});
+   
+    const originalWriteFile = host.writeFile;
+    host.writeFile = (fileName, content, _writeBOM, _onError, sourceFiles) => {
+        console.log(`writing file '${fileName}', content length ${content.length}` 
+            + `, source file count: ${sourceFiles ? sourceFiles.length : 'no source files'}`);
+        originalWriteFile(fileName, content, _writeBOM, _onError, sourceFiles);
+    };
+   
+    const program = ts.createProgram({
+        rootNames: ['src/server/index.ts'],
+        options: typescriptServerConfig,
+        host: host,
+    });
+
+    const emitResult = program.emit();
+    console.log('emit ' + (emitResult.emitSkipped ? 'failed' : 'success'));
+    for (const { file, start, messageText } of ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics)) {
+        if (file) {
+            const { line, character } = file.getLineAndCharacterOfPosition(start);
+            process.stdout.write(`${file.fileName} (${line + 1},${character + 1}): `);
+        }
+        console.log(ts.flattenDiagnosticMessageText(messageText, '\n'));
+    }
+
+    webpack({
+        mode: 'development',
+        entry: './custom_build/index.js',
+        target: 'node',
+        output: {
+            path: path.join(projectDirectory, 'build'),
+            filename: 'server.js',
+        },
+        externals: Object.keys(nodePackage.dependencies)
+            .reduce((x, e) => { x[e] = 'commonjs ' + e; return x; }, {}),
+    }, (err, stats) => {
+        if (err) {
+            console.log('webpack error: ');
+            console.log(err);
+            return;
+        }
+
+        const statData = stats.toJson();
+        if (stats.hasErrors()) {
+            console.log('errors: ', statData.errors);
+            return;
+        }
+        if (stats.hasWarnings()) {
+            console.log('warnings: ', statData.warnings.join('\n'));
+        }
+
+        const { version, hash, time, builtAt, assets, chunks, modules } = statData;
+        console.log({ version, hash, time, builtAt });
+
+        for (const asset of assets) {
+            console.log(`asset ${asset.name} size ${asset.size}`);
+        }
+        for (const chunk of chunks) {
+            console.log(`chunk#${chunk.id}: ${chunk.names.join(', ')}`);
+        }
+        for (const module of modules) {
+            console.log(`module#${module.id}: '${module.name}', size ${module.size}`);
+        }
+    });
+}
+
+const { targetName, targetType, targetOptions } = checkProcessArguments();
+
+if (targetName == 'server') {
+    buildServer(targetOptions);
+}
+
---
+++ scripts/myts.js
@@ -0,0 +1,95 @@
+const fs = require('fs');
+const path = require('path');
+const ts = require("typescript");
+
+function compile(fileNames, options) {
+    const program = ts.createProgram(fileNames, options);
+    const emitResult = program.emit();
+
+    ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics).forEach(diagnostic => {
+        if (diagnostic.file) {
+            const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start);
+            const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
+            console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
+        } else {
+            console.log(`${ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n')}`);
+        }
+    });
+
+    process.exit(emitResult.emitSkipped ? 1 : 0);
+}
+
+function getAllFiles(directory, predicate) {
+    const results = [];
+
+    function impl(d) {
+        const items = fs.readdirSync(d, { withFileTypes: true });
+        
+        results.push(...items.filter(i => i.isFile() && predicate(i.name)).map(i => path.join(d, i.name)));
+        for (const sub of items.filter(i => i.isDirectory()).map(i => path.join(d, i.name))) {
+            impl(sub);
+        }
+    }
+
+    impl(directory);
+    return results;
+}
+
+const projectDirectory = process.cwd();
+const formatHost = {
+    getCanonicalFileName: f => path.relative(projectDirectory, f),
+    getCurrentDirectory: () => projectDirectory,
+    getNewLine: () => '\n',
+};
+
+function reportDiagnostic(diagnostic) {
+    console.error('Error', diagnostic.code, ':', ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n'));
+}
+function reportWatchStatusChanged(diagnostic) {
+    console.info(ts.formatDiagnostic(diagnostic, formatHost));
+}
+
+function watch(fileNames, config) {
+    const host = ts.createWatchCompilerHost(fileNames, config, 
+        ts.sys, ts.createEmitAndSemanticDiagnosticsBuilderProgram, reportDiagnostic, reportWatchStatusChanged);
+   
+    const originalCreateProgram = host.createProgram;
+    host.createProgram = (rootNames, options, host, oldProgram) => {
+        console.log('** before create program');
+        return originalCreateProgram(rootNames, options, host, oldProgram);    
+    };
+
+    const originalAfterProgramCreate = host.afterProgramCreate;
+    host.afterProgramCreate = program => {
+        console.log('** after create program');
+        if (originalAfterProgramCreate) originalAfterProgramCreate(program);
+    };
+
+    ts.createWatchProgram(host);
+}
+
+const rootDirectory = path.join(process.cwd(), 'src/server');
+const files = getAllFiles(rootDirectory, name => path.extname(name) == '.ts');
+const config =  {
+    module: ts.ModuleKind.CommonJS,
+    allowSyntheticDefaultImports: true,
+    target: ts.ScriptTarget.ES2017,
+    noEmitOnError: true,
+    noErrorTruncation: true,
+    noFallthroughCasesInSwitch: true,
+    noImplicitAny: true,
+    noImplicitReturns: true,
+    noImplicitThis: true,
+    noUnusedLocals: true,
+    noUnusedParameters: true,
+    strict: true,
+    strictFunctionTypes: true,
+    strictBindCallApply: true,
+    strictNullChecks: true,
+    strictPropertyInitialization: true,
+    outDir: './custom_build',
+};
+
+compile(['src/server/index.ts'], config);
+// watch(files, config);
+
