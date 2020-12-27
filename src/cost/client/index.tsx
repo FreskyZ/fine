fb1e91ab74d54ac02fd3808b36007f44eb9cd285
Fresky Han <FreskyZ@outlook.com>
2020-12-28 00:36:23 +0800

    [wimm] update style

--- src/cost/client/index.tsx
+++ src/cost/client/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { Button, Tag, Modal, Select, Input, InputNumber, Spin, Popconfirm, message } from 'antd';
-import { CloseOutlined, EditOutlined }  from '@ant-design/icons';
+import { Button, Tag, Modal, Radio, Input, InputNumber, Spin, Popconfirm, message } from 'antd';
+import { CloseOutlined, PlusOutlined }  from '@ant-design/icons';
 import { DatePicker, TimePicker } from './dayjs-picker';
 import dayjs from 'dayjs';
 import type { Record } from '../api';
@@ -22,30 +22,32 @@ const RecordRow: React.FC<RecordRowProps> = ({ record, handleEdit, handleDeleteF
         });
     }
 
-    return <div className='record-row'><Spin spinning={loading}>
+    const displayType = record.type == 'cost' ? '支出' : record.type == 'income' ? '收入' : '转移';
+
+    return <div className='record-display' onClick={handleEdit}><Spin spinning={loading}>
         <span className='record-title'>{record.title}</span>
-        <span className='record-tags'>{[record.type as string].concat(record.tags).map(tag => <Tag className='record-tag'><span>{tag}</span></Tag>)}</span>
+        <span className='record-tags'>{[displayType].concat(record.tags).map(tag => <Tag className='record-tag'><span>{tag}</span></Tag>)}</span>
         <span className='record-time'>{record.time}</span>
         <span className='record-amount'>&#x00A5;{record.amount}</span>
-        <Button icon={<EditOutlined />} onClick={handleEdit} />
-        <Popconfirm title='确定要删除这条记录吗？' onConfirm={handleDelete}>
+        <Popconfirm title='确定要删除这条记录吗？' cancelText='取消' okText='确认' onConfirm={handleDelete}>
             <CloseOutlined />
         </Popconfirm>
     </Spin></div>;
 }
 
 // TODO: try antd 4 form later
-type AddModalProps = { handleFinish: (newRecord: Record) => any };
-const AddModal: React.FC<AddModalProps> = ({ handleFinish }) => {
+type AddModalProps = { handleCancel: () => void, handleFinish: (newRecord: Record) => any };
+const AddModal: React.FC<AddModalProps> = ({ handleCancel, handleFinish }) => {
     const [loading, setLoading] = useState(false);
     const [title, setTitle] = useState('');
+    const [tags, setTags] = useState('');
     const [type, setType] = useState<Record['type']>('cost');
     const [time, setTime] = useState(dayjs());
-    const [amount, setAmount] = useState(0);
+    const [amount, setAmount] = useState<number>(undefined);
 
     const handleSave = () => {
         setLoading(true);
-        api.addRecord({ id: 0, title, type, tags: [], amount, time: time.format('YYYYMMDD-HHmmss') }).then(result => {
+        api.addRecord({ id: 0, title, type, tags: tags ? tags.split(',') : [], amount: amount || 0, time: time.format('YYYYMMDD-HHmmss') }).then(result => {
             message.info('保存成功');
             result.time = time.format('YYYY-MM-DD HH:mm:ss');
             handleFinish(result);
@@ -56,19 +58,23 @@ const AddModal: React.FC<AddModalProps> = ({ handleFinish }) => {
     }
 
     return <Modal 
-        title='添加记录'
+        title='添加'
         visible={true}
+        className='record-edit'
         maskClosable={false}
-        footer={<Button loading={loading} onClick={handleSave}>保存</Button>}>
-        <Input value={title} disabled={loading} onChange={e => setTitle(e.target.value)} />
-        <Select value={type} disabled={loading} onChange={(newValue: Record['type']) => setType(newValue)}>
-            <Select.Option value='cost'>支出</Select.Option>
-            <Select.Option value='income'>收入</Select.Option>
-            <Select.Option value='transfer'>转移</Select.Option>
-        </Select>
-        <DatePicker value={time} disabled={loading} onChange={newValue => setTime(time.year(newValue.year()).month(newValue.month()).day(newValue.day()))} />
-        <TimePicker value={time} disabled={loading} onChange={newValue => setTime(time.hour(newValue.hour()).minute(newValue.minute()).day(newValue.day()))} />
-        <InputNumber value={amount} disabled={loading} onChange={((newValue: number) => setAmount(newValue)) as any} />
+        centered={true}
+        onCancel={handleCancel}
+        footer={<Button loading={loading} disabled={!title || !amount} size='small' onClick={handleSave}>保存</Button>}>
+        <Radio.Group className='record-type' value={type} disabled={loading} onChange={e => setType(e.target.value)}>
+            <Radio value='cost'>支出</Radio>
+            <Radio disabled={true} value='income'>收入</Radio>
+            <Radio disabled={true} value='transfer'>转移</Radio>
+        </Radio.Group>
+        <Input className='record-title' value={title} disabled={loading} onChange={e => setTitle(e.target.value)} />
+        <Input className='record-tags' value={tags} placeholder='标签，逗号分隔' disabled={loading} onChange={e => setTags(e.target.value)} />
+        <InputNumber className='record-amount' value={amount} disabled={loading} onChange={((newValue: number) => setAmount(newValue)) as any} />
+        <DatePicker className='record-date' value={time} disabled={loading} onChange={newValue => setTime(time.year(newValue.year()).month(newValue.month()).date(newValue.date()))} />
+        <TimePicker className='record-time' value={time} disabled={loading} onChange={newValue => setTime(time.hour(newValue.hour()).minute(newValue.minute()).second(newValue.second()))} />
     </Modal>;
 }
 
@@ -81,13 +87,16 @@ function App() {
     }, []);
 
     return <>
-        {records.map(record => <RecordRow 
+        <header>WIMM?</header>
+        {records.sort((r1, r2) => dayjs(r1.time).isBefore(dayjs(r2.time)) ? 1 : dayjs(r1.time).isAfter(dayjs(r2.time)) ? -1 : 0).map(record => <RecordRow 
             key={record.id}
             record={record}
-            handleEdit={() => {}}
+            handleEdit={() => message.info('TODO')}
             handleDeleteFinish={() => setRecords(records.filter(r => r.id != record.id))}/>)}
-        <Button onClick={() => setAddModalVisible(true)}>添加</Button>
-        {addModalVisible && <AddModal handleFinish={newRecord => { setRecords(records.concat([newRecord])); setAddModalVisible(false) }} />}
+        <div className='add-container'><Button icon={<PlusOutlined />} shape='circle' onClick={() => setAddModalVisible(true)} /></div>
+        {addModalVisible && <AddModal 
+            handleCancel={() => setAddModalVisible(false)}
+            handleFinish={newRecord => { setRecords(records.concat([newRecord])); setAddModalVisible(false) }} />}
     </>;
 }
 
